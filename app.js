75cd95cc3bf5f50056495ac80a5906a5a8b8ebf3
Jake <jake.a.reddy@gmail.com>
2021-08-09 21:36:17 +0300

    cleanup and added logger utility

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
+//Local modules
 const config = require('./utils/config')
+const logger = require('./utils/logger')
+
+//Non-native modules
 const express = require('express')
 const mongoose = require('mongoose')
-
-//non-native imports
-const lineReader = require('line-reader')
 const morgan = require('morgan')
 const cors = require('cors')
 
 //Router imports
-const casesRouter = require('./controllers/cases')
 const vaccinationsRouter = require('./controllers/vaccinations')
 const ordersRouter = require('./controllers/orders')
 const dataRouter = require('./controllers/data')
@@ -17,7 +17,7 @@ const dataRouter = require('./controllers/data')
 const app = express()
 
 //Mongoose connecting
-console.log('Connecting to: ', config.MONGODB_URI)
+logger.info('Connecting to: ', config.MONGODB_URI)
 mongoose
   .connect(config.MONGODB_URI, {
     useNewUrlParser: true,
@@ -25,15 +25,17 @@ mongoose
     useFindAndModify: false,
     useCreateIndex: true,
   })
-  .then(() => console.log('connecting to mongoDB'))
-  .catch((error) => console.log('Error connecting to MongoDB: ', error.message))
+  .then(() => logger.info('connecting to mongoDB'))
+  .catch((error) =>
+    logger.error('Error connecting to MongoDB: ', error.message)
+  )
 
+//Middleware
 app.use(cors())
 app.use(express.json())
 app.use(morgan('common'))
 
 //Router initialization
-
 app.use('/api/data', dataRouter)
 app.use('/api/vaccinations', vaccinationsRouter)
 app.use('/api/orders', ordersRouter)
