f221e86c1eeb782d5a285efde035287e55618ae8
Jake <jake.a.reddy@gmail.com>
2021-08-10 23:21:57 +0300

    moved helper func into own modules

---
+++ utils/controller-helpers.js
@@ -0,0 +1,42 @@
+const getDirectory = (brand) => {
+  switch (brand) {
+    case 'Antiqua':
+      return path.join(__dirname, '..', 'data', 'Antiqua.source')
+    case 'Solar':
+      return path.join(__dirname, '..', 'data', 'SolarBuddhica.source')
+    case 'Zerpfy':
+      return path.join(__dirname, '..', 'data', 'Zerpfy.source')
+    default:
+      return null
+  }
+}
+
+const toOrder = (lineObject) => {
+  const order = new Order({
+    _id: lineObject['id'],
+    orderNumber: lineObject['orderNumber'],
+    responsiblePerson: lineObject['responsiblePerson'],
+    healthCareDistrict: lineObject['healthCareDistrict'],
+    vaccine: lineObject['vaccine'],
+    injections: lineObject['injections'],
+    arrived: lineObject['arrived'],
+  })
+  return order
+}
+
+const toVaccination = (lineObject) => {
+  const vaccination = new Vaccination({
+    _id: lineObject['vaccination-id'],
+    sourceBottle: lineObject['sourceBottle'],
+    gender: lineObject['gender'],
+    vaccinationDate: lineObject['vaccinationDate'],
+  })
+
+  return vaccination
+}
+
+module.exports = {
+  getDirectory,
+  toOrder,
+  toVaccination,
+}
